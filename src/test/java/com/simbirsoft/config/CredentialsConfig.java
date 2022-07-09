ba303cac818180146e33cebdd34e78126fc6bfdf
SimpleCat1 <104307995+SimpleCat1@users.noreply.github.com>
2022-07-09 13:52:52 +0400

    delete comment

--- src/test/java/com/simbirsoft/config/CredentialsConfig.java
+++ src/test/java/com/simbirsoft/config/CredentialsConfig.java
@@ -3,8 +3,6 @@ package com.simbirsoft.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({"classpath:config/${testType}.properties", "classpath:config/local.properties"})
-//последовательно идет по коду, и выбирает ресур, тот , которые подошел, если не указал, то берет следующий.Очень удобно
-//Запускает и через зеленую стрелку и через консоль и по одиночке и с указаение .properties файла
 public interface CredentialsConfig extends Config {
     String login();
     String password();
